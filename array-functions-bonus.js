8ee5107ba384da03fb7c08ac1fda9c9c8781fd3a
Bhon Bunnag <bhonris@gmail.com>
2022-06-15 20:26:42 -0700

    Add new function

---
+++ array-functions-bonus.js
@@ -0,0 +1,20 @@
+// more practice
+// getSumOfPositiveElement(Array<number>) => number
+// Sum of every positive element
+function getSumOfPositiveElement(numArray){
+  return -1
+}
+// getSumOfPositiveElement([ 1, -4, 12, 0, -3, 29, -150]) => 42
+
+// getSumOfPositiveElement(Array<number>) => {mean: number, median: number}
+// calculate the median and mean
+function getMedianAndMean(numArray){
+  return -1
+}
+// getSumOfPositiveElement([12, 46, 32, 64, 8]) => { mean: 38.5, median: 32 }
+
+// getNameInitials(text: string) => string
+function getNameInitials(text){
+  return ""
+}
+// getNameInitials('George Raymond Richard Martin') => 'GRRM'
