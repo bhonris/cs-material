6536b685ca4096e0ec6a8aa2a969acb747e13bd8
Bhon Bunnag <bhonris@gmail.com>
2022-06-15 19:47:30 -0700

    Array Functions

---
+++ array-functions.js
@@ -0,0 +1,65 @@
+// getEvenNumbers(numArray: Array<number>) => Array<number>
+// consumes a list, and returns a list containing only the even numbers in the consumed list
+function getEvenNumbers(numArray){
+  return []
+}
+// Example:
+// getEvenNumbers([12,5,7,2,7,8]) => [12,2,8]
+// getEvenNumbers([1,3,5]) => []
+// getEvenNumbers([2,4,8]) => [2,4,8]
+
+
+// doubleNumbers(numArray: Array<number>) => Array<number>
+// consumes a list, and returns a list which doubles all the original value
+function doubleNumbers(numArray){
+  return []
+}
+// Example:
+// getEvenNumbers([1,3,5]) => [2,6,10]
+// getEvenNumbers([2,4,8]) => [4,8,16]
+
+
+// sumNumbers(numArray: Array<number>) => number
+// consumes a list, returns the sum of the entire list
+function sumNumbers(numArray){
+  return []
+}
+// Example:
+// getEvenNumbers([1,3,5]) => 9
+// getEvenNumbers([2,4,8]) => 28
+
+
+
+
+// Assignment
+
+// consumes a list of number representing indexes, returns the fibonacci number representing that index
+function getFibonacciOfIndex(indexArray){
+  return []
+}
+// Example 
+// getFibonacciOfIndex([0,8,6]) => [0,21,8]
+
+// getNameOfExpensiveFruit(costThreshold: number) => Array<string>
+// consumes a list, and returns a list of name of fruits that exceed or is equal to price threshold
+function getNameOfExpensiveFruit(costThreshold){
+  const FRUIT = [
+    {name: "apple", cost: 10},
+    {name: "pear", cost: 20},
+    {name: "banana", cost: 30},
+    {name: "", cost: -1} 
+  ]
+  return []
+}
+// Example:
+// getNameOfExpensiveFruit(15) => ["pear", "banana"]
+// getNameOfExpensiveFruit(30) => ["banana"]
+// getNameOfExpensiveFruit(50) => []
+
+// getMin(anArray: Array<number>) => number
+// Consumes a list of number. returns the smallest number
+function getMin(anArray) {
+  return -1
+}
+// Example:
+// getMin([-1,5,7]) => -1
