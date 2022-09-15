03906c0942cf57bf5505a5a48651d17393ba81bd
Bhon Bunnag <bhonris@gmail.com>
2022-09-14 19:38:10 -0700

    new search

---
+++ search.js
@@ -0,0 +1,77 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.descendants = [];
+  }
+
+  addChild(childNode) {
+    this.descendants.push(childNode)
+  }
+}
+
+class Queue {
+  constructor(){
+    this.value = [] 
+  }
+  add(value){
+    this.value.push(value)
+  }
+  addMany(value){
+    this.value.push(...value)
+  }
+  get(){
+    return this.value.shift()
+  }
+  length(){
+    return this.value.length
+  }
+}
+
+class Stack {
+  constructor(){
+    this.value = [] 
+  }
+  add(value){
+    this.value.unshift(value)
+  }
+  addMany(value){
+    this.value.unshift(...value)
+  }
+  get(){
+    return this.value.shift()
+  }
+  length(){
+    return this.value.length
+  }
+}
+
+
+const kingCharles = new TreeNode("Charles")
+// const elizabeth = new TreeNode("Elizabeth")
+const william = new TreeNode("William")
+const george = new TreeNode("George")
+const charlotte = new TreeNode("Charlotte")
+const louis = new TreeNode("Louis")
+const harry = new TreeNode("Harry")
+const archie = new TreeNode("Archie")
+const lilibet = new TreeNode("Lilibet")
+// const andrew = new TreeNode("Andrew")
+// const beatrice = new TreeNode("Beatrice")
+// const sienna = new TreeNode("Sienna")
+// const eugenie = new TreeNode("Eugenie")
+// const august = new TreeNode("August")
+kingCharles.addChild(william)
+kingCharles.addChild(harry)
+
+william.addChild(george)
+william.addChild(charlotte)
+william.addChild(louis)
+
+harry.addChild(archie)
+harry.addChild(lilibet)
+
+function depthFirstSearch(root) {
+
+} 
+
+console.log(depthFirstSearch(kingCharles))
